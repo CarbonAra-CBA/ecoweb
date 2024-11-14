3ffbcb21d1e205b70b4374099446843a43c40e4b
magatia3113 <gmltn3113@naver.com>
2024-11-15 01:07:46 +0900

    11/15

---
+++ 메인파일/test.py
@@ -0,0 +1,26 @@
+x=7.5
+
+print(type(x)==float) 
+
+num= int (input("정수를 입력 하세요:"))
+
+if   num > 0 : 
+    print("양수입니다.")
+elif num < 0:
+    print("음수입니다.")
+else : 
+    print("0입니다.")
+
+
+
+for i in range(1,11) : 
+    print(f"{i}의 제곱은 {i**2}")
+
+
+numbers = [ 1,2,3,4,5]
+
+total = sum(numbers)
+
+print("합:total")
+
+
